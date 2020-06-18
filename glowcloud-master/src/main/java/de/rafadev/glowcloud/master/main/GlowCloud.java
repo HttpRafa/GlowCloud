68cd6b9e2ac1a4c5730fdd2469ab5663bbbcac83
Rafael <60099368+HttpRafa@users.noreply.github.com>
2020-06-18 21:52:44 +0200

    Adding the cloud module system

--- glowcloud-master/src/main/java/de/rafadev/glowcloud/master/main/GlowCloud.java
+++ glowcloud-master/src/main/java/de/rafadev/glowcloud/master/main/GlowCloud.java
@@ -10,11 +10,16 @@ package de.rafadev.glowcloud.master.main;
 
 import de.rafadev.glowcloud.lib.interfaces.IGlowCloudObject;
 import de.rafadev.glowcloud.lib.logging.CloudLogger;
+import de.rafadev.glowcloud.lib.network.address.NetworkAddress;
 import de.rafadev.glowcloud.lib.scheduler.GlowScheduler;
 import de.rafadev.glowcloud.master.command.CommandManager;
 import de.rafadev.glowcloud.master.command.commands.*;
 import de.rafadev.glowcloud.master.file.FileManager;
 import de.rafadev.glowcloud.master.group.GroupManager;
+import de.rafadev.glowcloud.master.module.manager.CloudModuleManager;
+import de.rafadev.glowcloud.master.network.manager.NetworkManager;
+
+import java.io.File;
 
 public class GlowCloud {
 
@@ -24,11 +29,15 @@ public class GlowCloud {
     private CommandManager commandManager;
     private FileManager fileManager;
     private GroupManager groupManager;
+    private NetworkManager networkManager;
+    private CloudModuleManager moduleManager;
 
     private GlowScheduler scheduler;
 
     public GlowCloud() throws Exception {
         glowCloud = this;
+
+        long startTime = System.currentTimeMillis();
 
         /*
         Init FileSystem
@@ -49,6 +58,7 @@ public class GlowCloud {
          */
         logger = new CloudLogger();
 
+        logger.info("Starting SchedulerService...");
         /*
         Loading SchedulerService
          */
@@ -59,12 +69,14 @@ public class GlowCloud {
          */
         groupManager = new GroupManager();
         commandManager = new CommandManager(logger);
+        moduleManager = new CloudModuleManager();
 
         /*
         Register All Command
          */
         commandManager.registerCommand(new StopCommand("stop", null, "Stops the GlowCloud application"));
         commandManager.registerCommand(new StopCommand("exit", null, "Stops the GlowCloud application"));
+        commandManager.registerCommand(new ReloadCommand("reload", "§8<§eall, configs§8>", "Reload the GlowCloud application"));
         commandManager.registerCommand(new HelpCommand("help", null, "Displays all executable commands"));
         commandManager.registerCommand(new DebugCommand("debug", null, "Enable the DebugMode for the Cloud"));
         commandManager.registerCommand(new CreateCommand("create", "§8<§eServerGroup§8>", "Creates a servergroup or a wrapper"));
@@ -74,10 +86,74 @@ public class GlowCloud {
         commandManager.registerCommand(new ScreenCommand("screen", "§8<§eServer§8>", "View the console messages of a server"));
         commandManager.registerCommand(new CopyCommand("copy", "§8<§eServer§8> §8<§eTemplate§8>", "Copy all data from a server into the template"));
         commandManager.registerCommand(new CommandCommand("command", "§8<§eServer§8> <§eCommand[]...§8>", "Executes a command on a server"));
+
+        /*
+         Start GlowCloud Services
+         */
+
+        /*
+         Load all Modules
+         */
+        moduleManager.loadModules();
+
+        /*
+        Starting NetworkSystem
+         */
+        networkManager = new NetworkManager();
+        networkManager.startNetworkServer(new NetworkAddress("127.0.0.1", fileManager.getConfig().get("cloud").getAsJsonObject().get("port").getAsInt()));
+
+        /*
+         Load all ServerGroup
+         */
+        groupManager.loadGroups();
+
+        /*
+         Enable all Modules
+         */
+        moduleManager.enableModules();
+
+        /*
+         Start Cloud main actions
+         */
+        logger.info("The cloud was started in §e" + (System.currentTimeMillis() - startTime) + " §7ms§8.");
+        logger.info("For help, write §8\"§ehelp§8\".");
+    }
+
+    public void reloadAll() {
+
+        GlowCloud.getGlowCloud().getLogger().warning("There may be serious errors when reloading these are not supported§8.");
+        GlowCloud.getGlowCloud().getLogger().info("Reloading the cloud§8...");
+
+        /*
+        Disable all Modules
+         */
+        moduleManager.disableModules();
+
+        /*
+         Load all Modules
+         */
+        moduleManager.loadModules();
+
+        /*
+         Reload all ServerGroup
+         */
+        groupManager.reload();
+
+        /*
+         Enable all Modules
+         */
+        moduleManager.enableModules();
+
     }
 
     public void shutdown() {
         System.exit(0);
+    }
+
+    public void exit() {
+        if(!fileManager.isFistStart()) {
+            logger.info("Cloud is shutting down...");
+        }
     }
 
     public CommandManager getCommandManager() {
