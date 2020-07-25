9e87db12a5ab6d7074355d473f20c47fa9f57ae3
Rafael <60099368+HttpRafa@users.noreply.github.com>
2020-07-25 19:22:40 +0200

    Updating the packets

--- glowcloud-master/src/main/java/de/rafadev/glowcloud/master/network/packet/in/PacketInRegisterServer.java
+++ glowcloud-master/src/main/java/de/rafadev/glowcloud/master/network/packet/in/PacketInRegisterServer.java
@@ -8,14 +8,28 @@ package de.rafadev.glowcloud.master.network.packet.in;
 //
 //------------------------------
 
+import com.google.gson.Gson;
+import de.rafadev.glowcloud.lib.classes.server.CloudServer;
 import de.rafadev.glowcloud.lib.document.Document;
+import de.rafadev.glowcloud.lib.logging.ProcessBar;
+import de.rafadev.glowcloud.lib.network.protocol.ProtocolSender;
 import de.rafadev.glowcloud.lib.network.protocol.packet.PacketInHandler;
 import de.rafadev.glowcloud.lib.network.protocol.packet.PacketSender;
+import de.rafadev.glowcloud.master.main.GlowCloud;
+import de.rafadev.glowcloud.master.server.classes.OnlineCloudServer;
+import de.rafadev.glowcloud.master.wrapper.classes.ConnectedCloudWrapper;
 
 public class PacketInRegisterServer extends PacketInHandler {
 
     @Override
     public void handle(Document document, PacketSender packetSender) {
+
+        if(packetSender instanceof ConnectedCloudWrapper) {
+
+            CloudServer cloudServer = new Gson().fromJson(document.get("cloudServer").getAsJsonObject(), CloudServer.class);
+            GlowCloud.getGlowCloud().getServerManager().registerServer(cloudServer);
+
+        }
 
     }
 
--- glowcloud-master/src/main/java/de/rafadev/glowcloud/master/network/packet/in/PacketInUnRegisterServer.java
+++ glowcloud-master/src/main/java/de/rafadev/glowcloud/master/network/packet/in/PacketInUnRegisterServer.java
@@ -8,14 +8,25 @@ package de.rafadev.glowcloud.master.network.packet.in;
 //
 //------------------------------
 
+import com.google.gson.Gson;
+import de.rafadev.glowcloud.lib.classes.server.CloudServer;
 import de.rafadev.glowcloud.lib.document.Document;
 import de.rafadev.glowcloud.lib.network.protocol.packet.PacketInHandler;
 import de.rafadev.glowcloud.lib.network.protocol.packet.PacketSender;
+import de.rafadev.glowcloud.master.main.GlowCloud;
+import de.rafadev.glowcloud.master.wrapper.classes.ConnectedCloudWrapper;
 
 public class PacketInUnRegisterServer extends PacketInHandler {
 
     @Override
     public void handle(Document document, PacketSender packetSender) {
+
+        if(packetSender instanceof ConnectedCloudWrapper) {
+
+            CloudServer cloudServer = new Gson().fromJson(document.get("cloudServer").getAsJsonObject(), CloudServer.class);
+            GlowCloud.getGlowCloud().getServerManager().unRegisterServer(cloudServer);
+
+        }
 
     }
 
